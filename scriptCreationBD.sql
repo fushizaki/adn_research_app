e76b0b2e2cdc7f86d0acfb1ada0f39457a34183b
fushizaki <ilona.cosme-vinou@etu.univ-orleans.fr>
2025-09-18 12:15:30 +0200

    Commit du trigger (vérifier que les habilitations sont toutes requises au sein de l'équipe sur la plateforme)

--- scriptCreationBD.sql
+++ scriptCreationBD.sql
@@ -346,22 +346,6 @@ INSERT INTO RAPPORTER (idEchant, idCampagne) VALUES
 (5, 10);
 
 
--- OK : Plateforme 3 (habilitations 1 et 3), Campagne 6 participants ont habilitations 1 et 3
-INSERT INTO PLANNIFIER (idPlateforme, idCampagne) VALUES (3, 6);
-
--- KO : Plateforme 5 (habilitations 1 et 4), Campagne 2 participants (non habilités 4)
-INSERT INTO PLANNIFIER (idPlateforme, idCampagne) VALUES (5, 2);
-
--- OK : Plateforme 7 (habilitations 1, 2, 3), Campagne 3 avec participants habilités 1, 2, 3
-INSERT INTO PLANNIFIER (idPlateforme, idCampagne) VALUES (7, 3);
-
--- KO : Plateforme 8 (habilitation 4), Campagne 1 (participants pas habilités 4)
-INSERT INTO PLANNIFIER (idPlateforme, idCampagne) VALUES (8, 1);
-
-
--- TRIGGER : S’assurer que les personnes travaillant sur la plateforme sont habilité à le faire
-
-
 DELIMITER |
 
 CREATE TRIGGER verif_habilite_personnes
@@ -369,28 +353,58 @@ BEFORE INSERT ON PLANNIFIER
 FOR EACH ROW
 
 BEGIN
-
-    DECLARE hab_requises int;
-    DECLARE hab_people int;
-
+    DECLARE hab_requises INT;
+    DECLARE hab_people INT;
+    
     SELECT COUNT(*) INTO hab_requises
-    FROM DETENIR 
+    FROM DETENIR
     WHERE idPlateforme = NEW.idPlateforme;
+    
 
-    SELECT COUNT(DISTINCT idHabilitation) INTO hab_people
-    FROM PARTICIPER NATURAL JOIN HABILITER
-    WHERE PARTICIPER.idCampagne = NEW.idCampagne 
-    IN(
+    SELECT COUNT(DISTINCT h.idHabilitation) INTO hab_people
+    FROM PARTICIPER p
+    INNER JOIN HABILITER h ON p.idPersonne = h.idPersonne
+    WHERE p.idCampagne = NEW.idCampagne
+    AND h.idHabilitation IN (
         SELECT idHabilitation
         FROM DETENIR
-        WHERE idPlateforme=NEW.idPlateforme
+        WHERE idPlateforme = NEW.idPlateforme
     );
-
-    IF (hab_people < hab_requises) THEN
+    
+    IF hab_people < hab_requises THEN
         SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Erreur : Les personnes ne possèdent pas les habilitations requises';
+        SET MESSAGE_TEXT = 'Erreur : Léquipe ne possède pas toutes les habilitations requises';
     END IF;
 END|
 
 DELIMITER ;
 
+-- La série de TESTS suivantes est générér à l'IA
+
+-- Test 1: DEVRAIT RÉUSSIR
+-- Campagne 1: personnes 1,2,3,4 
+-- Personne 1: hab 1,2 | Personne 2: hab 3,4 | Personne 3: hab 1,3 | Personne 4: hab 2,4
+-- Plateforme 8 requiert seulement l'habilitation 4 -> OK (personne 2 et 4 l'ont)
+INSERT INTO PLANNIFIER (idPlateforme, idCampagne) VALUES (8, 1);
+
+-- Test 2: DEVRAIT ÉCHOUER  
+-- Campagne 8: personnes 3,7,11
+-- Personne 3: hab 1,3 | Personne 7: hab 1,2,3 | Personne 11: hab 1,2
+-- Plateforme 4 requiert hab 2,4 -> ÉCHEC (aucune personne n'a l'hab 4)
+INSERT INTO PLANNIFIER (idPlateforme, idCampagne) VALUES (4, 8);
+
+-- Test 3: DEVRAIT RÉUSSIR
+-- Campagne 4: personnes 13,14,15  
+-- Personne 13: hab 1,3 | Personne 14: hab 2,4 | Personne 15: hab 1,4
+-- Plateforme 6 requiert hab 2,3 -> OK (personne 14 a hab 2, personne 13 a hab 3)
+INSERT INTO PLANNIFIER (idPlateforme, idCampagne) VALUES (6, 4);
+
+-- Vérification des résultats
+SELECT 'Tests effectués:' AS info;
+SELECT p.nom AS plateforme, c.idCampagne, 'AJOUTÉ' AS statut
+FROM PLANNIFIER pl
+JOIN PLATEFORME p ON pl.idPlateforme = p.idPlateforme  
+JOIN CAMPAGNE c ON pl.idCampagne = c.idCampagne
+WHERE (pl.idPlateforme = 8 AND pl.idCampagne = 1)
+   OR (pl.idPlateforme = 4 AND pl.idCampagne = 8) 
+   OR (pl.idPlateforme = 6 AND pl.idCampagne = 4); 
