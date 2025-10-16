c2511630e95671f60a1629ccc202a15c1d3992c1
korasrar <celestin.maubert@etu.univ-orleans.fr>
2025-10-16 11:46:15 +0200

    fix : changement du nom de la variable D en tableau_d

--- algo/main.py
+++ algo/main.py
@@ -11,17 +11,17 @@ def distance_de_levenshtein(seq1: str, seq2: str) -> int:
     """
     len_seq1 = len(seq1)
     len_seq2 = len(seq2)
-    D = [[0] * (len_seq2 + 1) for _ in range(len_seq1 + 1)]
+    tableau_d = [[0] * (len_seq2 + 1) for _ in range(len_seq1 + 1)]
     for i in range(len_seq1 + 1):
-        D[i][0] = i
+        tableau_d[i][0] = i
     for j in range(len_seq2 + 1):
-        D[0][j] = j
+        tableau_d[0][j] = j
     for i in range(1, len_seq1 + 1):
         for j in range(1, len_seq2 + 1):
             if seq1[i - 1] == seq2[j - 1]:
                 cout_substitution = 0
             else:
                 cout_substitution = 1
-            D[i][j] = min(D[i - 1][j] + 1, D[i][j - 1] + 1,
-                          D[i - 1][j - 1] + cout_substitution)
-    return D[len_seq1][len_seq2]
+            tableau_d[i][j] = min(tableau_d[i - 1][j] + 1, tableau_d[i][j - 1] + 1,
+                          tableau_d[i - 1][j - 1] + cout_substitution)
+    return tableau_d[len_seq1][len_seq2]
