99c0da13a3cdacbeab844235cfd84f0296c2dba6
korasrar <celestin.maubert@etu.univ-orleans.fr>
2025-11-10 16:02:53 +0100

    add : docstring

--- algo/Espece.py
+++ algo/Espece.py
@@ -7,13 +7,28 @@ class Espece:
         self.especes_filles = especes_filles
 
     def est_averee(self):
+        """Vérifie si l'espèce est avérée (non hypothétique).
+
+        Returns:
+            bool: True si l'espèce est avérée, False sinon.
+        """
         return not self.est_hypothetique
 
     def add_espece_fille(self, espece_fille):
+        """Ajoute une espèce fille à l'espèce actuelle si elle est hypothétique.
+
+        Args:
+            espece_fille (Espece): L'espèce fille à ajouter.
+        """
         if self.est_hypothetique:
             self.especes_filles.append(espece_fille)
 
     def del_espece_fille(self, espece_fille):
+        """Supprime une espèce fille de l'espèce actuelle.
+
+        Args:
+            espece_fille (Espece): L'espèce fille à supprimer.
+        """
         self.especes_filles.remove(espece_fille)
 
     def __str__(self):
