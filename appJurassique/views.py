68d571deee649953cfd1e32bf4e2a6c6dc32c6d4
fushizaki <ilona.cosme-vinou@etu.univ-orleans.fr>
2025-11-17 18:08:33 +0100

    Ajout de l'environnement Flask + création template

--- appJurassique/views.py
+++ appJurassique/views.py
@@ -6,5 +6,8 @@ from appJurassique import app
 def index():
     return render_template('index.html', title='Accueil')
 
+def add_campagne():
+    return render_template('add_campagne.html', title='Ajouter une Campagne')
+
 if __name__ == "__main__":
     app.run()
