54e44dce145dbf1ce3ab08db50b52efa9f0dc939
korasrar <celestin.maubert@etu.univ-orleans.fr>
2025-11-19 00:41:12 +0100

    add : header no login

--- appJurassique/tailwind.config.js
+++ appJurassique/tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./templates/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-button': 'rgba(0, 255, 77, 1)',
+        'green-button-hover': 'rgba(0, 255, 77, 0.8)',
+      },
+      fontFamily: {
+        'nunito': ['Nunito', 'sans-serif'],
+        'exo': ['Exo', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
