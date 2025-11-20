3eb98258372d0c4164dd15239d46d7519d9cea4a
icosme <ilona.cosme-vinou@etu.univ-orleans.fr>
2025-11-20 21:20:02 +0100

    add : second section of my page + begininng of view instanciation

--- appJurassique/app.py
+++ appJurassique/app.py
@@ -1,8 +1,7 @@
+import os
 from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
-from flask_login import LoginManager
 
 load_dotenv()
 
@@ -10,12 +9,10 @@ app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 app.config.from_object('config')
 
-login_manager = LoginManager(app)
-
 db_user = os.getenv("DB_USER")
 db_password = os.getenv("DB_PASSWORD")
 db_name = os.getenv("DB_NAME")
 
 app.config[
-    "SQLALCHEMY_DATABASE_URI"] = f"mysql://{db_user}:{db_password}@localhost/{db_name}"
+    "SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{db_user}:{db_password}@localhost/{db_name}"
 db = SQLAlchemy(app)
--- appJurassique/views.py
+++ appJurassique/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from .app import app, db
+from .models import CAMPAGNE, LIEU_FOUILLE, PLATEFORME, PERSONNE, PARTICIPER
 
 @app.route('/')
 @app.route('/index/')
@@ -8,7 +9,11 @@ def index():
 
 @app.route('/add_campagne/')
 def add_campagne():
-    return render_template('add_campagne.html', title='Ajouter une Campagne')
+    lieux = LIEU_FOUILLE.query.all()
+    plateformes = PLATEFORME.query.all()
+    lieux = []
+    plateformes = []
+    return render_template('add_campagne.html', title='Ajouter une Campagne',campagne=None,lieux=lieux,plateformes=plateformes)
 
 @app.route('/login/')
 def login():
