df62886a5c36ffd55907d759f2ee24a1e4f6207c
korasrar <celestin.maubert@etu.univ-orleans.fr>
2025-11-20 11:23:48 +0100

    fix

--- appJurassique/tailwind.config.js
+++ appJurassique/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./templates/*.html'],
   theme: {
     extend: {
+      dropShadow: {
+        'button': '0 0 2px rgba(0, 255, 77, 1)'
+      },
       colors: {
         'green-button': 'rgba(0, 255, 77, 1)',
         'green-button-hover': 'rgba(0, 255, 77, 0.8)',
--- appJurassique/views.py
+++ appJurassique/views.py
@@ -4,11 +4,11 @@ from .app import app, db
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template('index.html', title='Accueil')
+    return render_template('index.html', title='Accueil', current_page='index')
 
 @app.route('/login/')
 def login():
-    return render_template('login.html', title='Login')
+    return render_template('login.html', title='Login', current_page='login')
 
 if __name__ == "__main__":
     app.run()
